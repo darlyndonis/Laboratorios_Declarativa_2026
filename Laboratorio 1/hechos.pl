6be72dfde8cb6a8c166d91ed6a01f4d0a702e1b0
darlyndonis <00083823@uca.edu.sv>
2026-08-25 22:27:42 -0600

    Ejercicio de Laboratorio 1 cambios finales

--- Laboratorio 1/hechos.pl
+++ Laboratorio 1/hechos.pl
@@ -34,7 +34,8 @@ enemigos(mutantes, la_superficie).
 no_tiene(los_bunkeres, enemigos).
 requiere(los_bunkeres, llaves).
 
-nivel_peligro(las_cuevas, alto).
+nivel_peligro(las_cuevas, dia, alto).
+nivel_peligro(las_cuevas, noche, alto).
 nivel_peligro(la_superficie, dia, medio).
 nivel_peligro(la_superficie, noche, alto).
 
