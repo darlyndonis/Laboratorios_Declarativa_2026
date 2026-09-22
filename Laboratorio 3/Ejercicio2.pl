df845de7f26fd2082da2877523b63375c1796ee6
darlyndonis <00083823@uca.edu.sv>
2026-09-22 15:12:31 -0600

    Agregando ejercicio 2 con dos casos base

--- Laboratorio 3/Ejercicio2.pl
+++ Laboratorio 3/Ejercicio2.pl
@@ -1,7 +1,11 @@
 % Dado un numero N, se debe sumar N con todos los numeros anteriores hasta llegar a 1.
+
+% Casos base
+sumar_con_anteriores(1, 1).
 sumar_con_anteriores(0, 0).
+% Codigo
 sumar_con_anteriores(N, Sum) :-
-    N > 0,
+    N > 1,
     N1 is N - 1,
     sumar_con_anteriores(N1, Sum1),
     Sum is Sum1 + N.
